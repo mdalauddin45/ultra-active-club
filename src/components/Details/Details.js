e404aed8781c4c0832bd25ad11eb9c905fb0145d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-29 00:43:04 +0600

    details also add

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -3,15 +3,38 @@ import './Details.css';
 
 const Details = () => {
     return (
-        <div className='details'>
+        <div className='all-details'>
+            <div className='details'>
             <div className='my-details'>
                 <img src="https://i1.sndcdn.com/avatars-gWjvzHQfdPnL6XRG-Vz3Txw-t500x500.jpg" alt="" />
             </div>
-
-            <div>
-             <h1>Md Ala Uddin</h1>
-             <h3>Chittagong, Bangladesh</h3>
-                </div>
+            <div className='my-name'>
+          <h4>Md Ala Uddin <br /> Chittagong, Bangladesh </h4>    
+        </div>
+        </div>
+        <div className='wha'>
+            <p>75 kg <br /> weight</p>
+            <p>5 <br /> Height</p>
+            <p>21 yrs <br /> Age</p>
+        </div>
+        <h3 className='break'>Add A Break
+</h3>
+<div className='five-btn'>
+    <button>10s</button>
+    <button>20s</button>
+    <button>30s</button>
+    <button>40s</button>
+    <button>50s</button>
+</div>
+<h3 className='break'>Exercise Details
+</h3>
+<div className='time'>
+   <p>Exercise time</p>
+</div>
+<div className='time'>
+    <p>Break time</p>
+</div>
+<button className='add-btn'>Activity Completed</button>
         </div>
     );
 };
