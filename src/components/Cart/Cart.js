6030921ffc1d6d318c1b947280526a058c608e5c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-29 11:49:35 +0600

    break time set

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,24 +2,36 @@ import React, { useState } from 'react';
 
 const Cart = ({times}) => {
     const [totalbt,setTotalbt]=useState(0);
+    const breakt= [10,20,30,40,50];
 
     let total = 0;
    for(const time of times){
      total = total + time;
     //  console.log(total);
    }
-   const secondsBtn=()=>{
-  const newbrektime= totalbt + 10;
-   setTotalbt(newbrektime);
+   const secondsBtn1=()=>{
+ setTotalbt(breakt[0]);
+   }
+   const secondsBtn2=()=>{
+ setTotalbt(breakt[1]);
+   }
+   const secondsBtn3=()=>{
+ setTotalbt(breakt[2]);
+   }
+   const secondsBtn4=()=>{
+ setTotalbt(breakt[3]);
+   }
+   const secondsBtn5=()=>{
+ setTotalbt(breakt[4]);
    }
     return (
        <div>
         <div className='five-btn'>
-    <button onClick={secondsBtn}>10s</button>
-    <button onClick={secondsBtn}>20s</button>
-    <button onClick={secondsBtn}>30s</button>
-    <button onClick={secondsBtn}>40s</button>
-    <button onClick={secondsBtn}>50s</button>
+    <button onClick={secondsBtn1}>10s</button>
+    <button onClick={secondsBtn2}>20s</button>
+    <button onClick={secondsBtn3}>30s</button>
+    <button onClick={secondsBtn4}>40s</button>
+    <button onClick={secondsBtn5}>50s</button>
 </div>
          <div>
             <h2>Exercise time: {total} seconds</h2>
