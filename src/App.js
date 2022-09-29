4e6dca48d753b317082157ce45e9bee9b9298334
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-29 19:30:20 +0600

    All Question clr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './components/Home/Home';
-import Quenstion from './components/Question/Question';
+import Question from './components/Question/Question';
+
 
 
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div>
       <Home></Home>
-     <Quenstion></Quenstion>
+      <Question></Question>
     </div>
   );
 }
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Question.css'
 
-const Quenstion = () => {
+const Question = () => {
     return (
-        <div className='question'>
-            <h1>How does React work?</h1>
+        <div>
+            <h1 className='h-text'>Question <hr /> </h1>
+            <div className='question'>
+           <div>
+           <h1>How does React work?</h1>
             <p> While building client-side apps, a team of Facebook developers realized that the DOM is slow (The Document Object Model (DOM) is an application programming interface (API) for HTML and XML documents. It defines the logical structure of documents and the way a document is accessed and manipulated.). So, to make it faster, React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.
 Unlike browser DOM elements, React elements are plain objects and are cheap to create. React DOM takes care of updating the DOM to match the React elements. The reason for this is that JavaScript is very fast and it’s worth keeping a DOM tree in it to speed up its manipulation.
 Although React was conceived to be used in the browser, because of its design it can also be used in the server with Node.js.</p>
+           </div>
+<div>
 <h1>What is the difference between state and props in React?</h1>
 <h3>State</h3>
 <p>
@@ -17,12 +22,16 @@ A Component manages its own state internally. Besides setting an initial state, 
 </p>
 <h3>Props</h3>
 <p>Props (short for properties) are a Component's configuration. They are received from above and immutable as far as the Component receiving them is concerned. A Component cannot change its props, but it is responsible for putting together the props of its child Components. Props do not have to just be data -- callback functions may be passed in as props.</p>
+</div>
+<div>
 <h1>How many the useEffect Hook Works (without Api data load(fetch))</h1>
 <h3>1.Run useEffect on State Change</h3>
 <h3>2.Run useEffect When a Prop Changes</h3>
 <h3>3.Re-fetch When Data Changes</h3>
+</div>
+        </div>
         </div>
     );
 };
 
-export default Quenstion;
+export default Question;
